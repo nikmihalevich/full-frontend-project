f01ff86372759ebc7180ed39204a8f8d0adbc60e
nikmihalevich <nikandr33@inbox.ru>
2023-05-10 12:31:34 +0300

    added jest and first test

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { t } from 'i18next';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -10,8 +11,10 @@ interface NavbarProps {
 export const Navbar = ({ className }: NavbarProps) => (
     <div className={classNames(cls.Navbar, {}, [className])}>
         <div className={cls.links}>
-            <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>Главная</AppLink>
-            <AppLink theme={AppLinkTheme.SECONDARY} to="/about">О сайте</AppLink>
+            <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>
+                {t('Главная')}
+            </AppLink>
+            <AppLink theme={AppLinkTheme.SECONDARY} to="/about">{t('О сайте')}</AppLink>
         </div>
     </div>
 );
