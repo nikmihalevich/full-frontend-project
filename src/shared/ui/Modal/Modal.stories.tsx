23f4473ec8bd7f28381e16f5a1652dfe4383d3a0
nikmihalevich <nikandr33@inbox.ru>
2023-05-22 23:11:29 +0300

    fix screens

--- src/shared/ui/Modal/Modal.stories.tsx
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -17,12 +17,12 @@ const Template: ComponentStory<typeof Modal> = (args) => <Modal {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
     isOpen: true,
-    children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae id perferendis perspiciatis provident, quia ratione sed voluptate. Error expedita vel veniam vero voluptatum. Culpa cum cupiditate distinctio eos id minima odio officia soluta veritatis vero. Architecto atque cumque laudantium molestias porro! Alias cum deleniti distinctio eius expedita fuga, illum nam nihil quia quo recusandae voluptatem. Aspernatur, nulla vel! Aperiam autem consectetur culpa eligendi hic ipsum iste libero mollitia nam odio, repellat, sunt suscipit veritatis. Architecto consectetur doloribus exercitationem hic incidunt iusto labore magnam, nesciunt nulla omnis possimus rem reprehenderit rerum sapiente sed sequi tempora temporibus tenetur ullam unde velit voluptatibus?',
+    children: 'Lorem ipsum dolor sit amet.',
 };
 
 export const Dark = Template.bind({});
 Dark.args = {
     isOpen: true,
-    children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae id perferendis perspiciatis provident, quia ratione sed voluptate. Error expedita vel veniam vero voluptatum. Culpa cum cupiditate distinctio eos id minima odio officia soluta veritatis vero. Architecto atque cumque laudantium molestias porro! Alias cum deleniti distinctio eius expedita fuga, illum nam nihil quia quo recusandae voluptatem. Aspernatur, nulla vel! Aperiam autem consectetur culpa eligendi hic ipsum iste libero mollitia nam odio, repellat, sunt suscipit veritatis. Architecto consectetur doloribus exercitationem hic incidunt iusto labore magnam, nesciunt nulla omnis possimus rem reprehenderit rerum sapiente sed sequi tempora temporibus tenetur ullam unde velit voluptatibus?',
+    children: 'Lorem ipsum dolor sit amet.',
 };
 Dark.decorators = [ThemeDecorator(Theme.DARK)];
