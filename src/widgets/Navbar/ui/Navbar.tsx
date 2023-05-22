4ec1c56f27563fa5e47261dd6e0f6d5a436b0fc8
nikmihalevich <nikandr33@inbox.ru>
2023-05-22 15:07:42 +0300

    upgrade sidebar, add button themes and visual tool, fix screens

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -11,10 +11,7 @@ interface NavbarProps {
 export const Navbar = ({ className }: NavbarProps) => (
     <div className={classNames(cls.Navbar, {}, [className])}>
         <div className={cls.links}>
-            <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>
-                {t('Главная')}
-            </AppLink>
-            <AppLink theme={AppLinkTheme.SECONDARY} to="/about">{t('О сайте')}</AppLink>
+            /
         </div>
     </div>
 );
