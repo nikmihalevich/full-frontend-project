b095100f896d4aa661160362e15c53ead9348279
nikmihalevich <nikandr33@inbox.ru>
2023-06-12 15:31:54 +0300

    add articles page

---
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage.async.tsx
@@ -0,0 +1,8 @@
+import { lazy } from 'react';
+
+export const ArticleDetailsPageAsync = lazy(() => new Promise((resolve) => {
+    setTimeout(() => {
+        // @ts-ignore
+        resolve(import('./ArticleDetailsPage'));
+    }, 1500);
+}));
---
+++ src/pages/ArticlesPage/ui/ArticlesPage.async.tsx
@@ -0,0 +1,8 @@
+import { lazy } from 'react';
+
+export const ArticlesPageAsync = lazy(() => new Promise((resolve) => {
+    setTimeout(() => {
+        // @ts-ignore
+        resolve(import('./ArticlesPage'));
+    }, 1500);
+}));
