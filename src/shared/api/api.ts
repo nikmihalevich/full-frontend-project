ba2c211ff06622d3a89340773202b5f4811e8c3b
nikmihalevich <nikandr33@inbox.ru>
2023-07-30 00:19:43 +0300

    fix api config

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -5,7 +5,12 @@ const baseURL = __IS_DEV__ ? 'http://localhost:8000' : '';
 
 export const $api = axios.create({
     baseURL: __API__,
-    headers: {
-        authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || '',
-    },
+});
+
+$api.interceptors.request.use((config) => {
+    if (config.headers) {
+        // eslint-disable-next-line no-param-reassign
+        config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+    }
+    return config;
 });
