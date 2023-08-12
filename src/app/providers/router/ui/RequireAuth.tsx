dbf2803063a922f4d39a7d355e1be3ff2c1539c9
nikmihalevich <nikandr33@inbox.ru>
2023-08-12 13:35:50 +0300

    add private route with required roles

--- src/app/providers/router/ui/RequireAuth.tsx
+++ src/app/providers/router/ui/RequireAuth.tsx
@@ -1,14 +1,36 @@
 import { Navigate, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { getUserAuthData } from 'entities/User';
+import { getUserAuthData, getUserRoles, UserRole } from 'entities/User';
 import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import { useMemo } from 'react';
 
-export function RequireAuth({ children }: { children: JSX.Element}) {
+interface RequireAuthProps {
+    children: JSX.Element;
+    roles?: UserRole[];
+}
+
+export function RequireAuth(props: RequireAuthProps) {
+    const { children, roles } = props;
     const auth = useSelector(getUserAuthData);
     const location = useLocation();
+    const userRoles = useSelector(getUserRoles);
+
+    const hasRequiredRoles = useMemo(() => {
+        if (!roles) {
+            return true;
+        }
+
+        return roles.some((requiredRole) => {
+            return userRoles?.includes(requiredRole);
+        });
+    }, [roles, userRoles]);
 
     if (!auth) {
         return <Navigate to={RoutePath.main} state={{ from: location }} replace />;
+    }
+
+    if (!hasRequiredRoles) {
+        return <Navigate to={RoutePath.forbidden} state={{ from: location }} replace />;
     }
 
     return children;
--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -9,4 +9,11 @@ export {
 export {
     UserSchema,
     User,
+    UserRole,
 } from './model/types/user';
+
+export {
+    isUserAdmin,
+    isUserManager,
+    getUserRoles,
+} from './model/selectors/roleSelectors';
