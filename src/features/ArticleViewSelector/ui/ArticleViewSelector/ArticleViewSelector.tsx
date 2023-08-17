1bab1ed2fc54a0a34bf13391269a2955af88c116
nikmihalevich <nikandr33@inbox.ru>
2023-08-17 21:47:49 +0300

    refactor entities

--- src/features/ArticleViewSelector/ui/ArticleViewSelector/ArticleViewSelector.tsx
+++ src/features/ArticleViewSelector/ui/ArticleViewSelector/ArticleViewSelector.tsx
@@ -1,5 +1,6 @@
 import { memo } from 'react';
 
+import { ArticleView } from '@/entities/Article';
 import ListIcon from '@/shared/assets/icons/list-icon.svg';
 import TiledIcon from '@/shared/assets/icons/tiled-icon.svg';
 import { classNames } from '@/shared/lib/classNames/classNames';
@@ -7,7 +8,6 @@ import { Button, ButtonTheme } from '@/shared/ui/Button';
 import { Icon } from '@/shared/ui/Icon';
 
 import cls from './ArticleViewSelector.module.scss';
-import { ArticleView } from '../../model/consts/articleConsts';
 
 interface ArticleViewSelectorProps {
     className?: string;
--- src/pages/ArticlesPage/ui/ArticlesPageFilters/ArticlesPageFilters.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPageFilters/ArticlesPageFilters.tsx
@@ -4,9 +4,11 @@ import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 
 import {
-    ArticleSortField, ArticleView, ArticleViewSelector, ArticleSortSelector,
-    ArticleType, ArticleTypeTabs,
+    ArticleSortField, ArticleView, ArticleType,
 } from '@/entities/Article';
+import { ArticleSortSelector } from '@/features/ArticleSortSelector';
+import { ArticleTypeTabs } from '@/features/ArticleTypeTabs';
+import { ArticleViewSelector } from '@/features/ArticleViewSelector';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
 import { useDebounce } from '@/shared/lib/hooks/useDebounce/useDebounce';
