33f0d447ba688c1fa26e97c3a7fd45dd1319f442
nikmihalevich <nikandr33@inbox.ru>
2023-08-17 14:39:48 +0300

    add documentation for project

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -21,14 +21,32 @@ export enum ButtonSize {
     XL = 'size_xl',
 }
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  className?: string;
-  theme?: ButtonTheme;
-  square?: boolean;
-  size?: ButtonSize;
-  disabled?: boolean;
-  children?: ReactNode;
-  fullWidth?: boolean;
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
+    className?: string;
+    /**
+     * Тема кнопки. Отвечает за визуал (в рамке, без стилей, противоположный теме приложения цвет и тд)
+     */
+    theme?: ButtonTheme;
+    /**
+     * Флаг, делающий кнопку квадратной
+     */
+    square?: boolean;
+    /**
+     * Размер кнопки в соответствии с дизайн системой
+     */
+    size?: ButtonSize;
+    /**
+     * Флаг, отвечающий за работу кнопки
+     */
+    disabled?: boolean;
+    /**
+     * Содержимое кнопки
+     */
+    children?: ReactNode;
+    /**
+     * Увеличивает кнопку на всю свободную ширину
+     */
+    fullWidth?: boolean;
 }
 
 export const Button = memo((props: ButtonProps) => {
