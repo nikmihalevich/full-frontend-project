4a78b9c2587426311f7976f0ed7339abfe4e9a31
nikmihalevich <nikandr33@inbox.ru>
2023-08-23 14:55:22 +0300

    upgrade storybook and update some redux packages

--- src/features/articleRating/ui/ArticleRating/ArticleRating.stories.tsx
+++ src/features/articleRating/ui/ArticleRating/ArticleRating.stories.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import withMock from 'storybook-addon-mock';
 
 import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
 
@@ -13,7 +12,6 @@ export default {
     argTypes: {
         backgroundColor: { control: 'color' },
     },
-    decorators: [withMock],
 } as ComponentMeta<typeof ArticleRating>;
 
 const Template: ComponentStory<typeof ArticleRating> = (args) => <ArticleRating {...args} />;
