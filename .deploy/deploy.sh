f239d83d65934de439e43e5ebc309b137d58f3bf
nikmihalevich <nikandr33@inbox.ru>
2023-08-24 20:15:44 +0300

    updated deploy script

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -1,5 +1,31 @@
-cd ~/frontend_course
-npm run build:prod
+PROJECT_NAME=$1 # parameter
+BUILD_FOLDER="build"
+DEST_PATH="/var/www/html"
 
-rm -rf ~/../var/www/frontend_course/html
-mv ~/frontend_course/build ~/../var/www/frontend_course/html
+if [[ -z $PROJECT_NAME ]]
+	then
+	  printf "Enter project folder name: "
+		read -r PROJECT_NAME
+		if [[ -z $PROJECT_NAME ]]
+    	then
+    		echo "exit..."
+    		exit
+    	fi
+	fi
+
+cd ~/"$PROJECT_NAME" || exit
+echo "Current path '${PWD}'"
+
+read -p "Pull from GitHub? (y/n): " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Fetching project from gitHub..."
+  echo
+  git pull
+fi
+
+echo "Start build project to '${BUILD_FOLDER}' folder..."
+npm run build:prod
+echo "Removing folder with renaming from '${BUILD_FOLDER}' to '${DEST_PATH}'..."
+rm -rf "$DEST_PATH" && mv "$BUILD_FOLDER" "$DEST_PATH"
