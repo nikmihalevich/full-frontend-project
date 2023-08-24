613ceb5848228d008ec8c1939925112087dbfab3
nikmihalevich <nikandr33@inbox.ru>
2023-08-24 21:15:25 +0300

    fix feature flags bug

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -3,15 +3,17 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { Counter } from '@/entities/Counter';
+import { getFeatureFlags } from '@/shared/lib/features';
 import { Page } from '@/widgets/Page';
 
 const MainPage = () => {
 	const { t } = useTranslation();
+	const isCounterEnabled = getFeatureFlags('isCounterEnabled');
 
 	return (
 		<Page data-testid="MainPage">
 			{t('Главная')}
-			<Counter />
+			{isCounterEnabled && <Counter />}
 		</Page>
 	);
 };
