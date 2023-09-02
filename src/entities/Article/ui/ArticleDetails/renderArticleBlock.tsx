6ef64efb62d600652973c7960f1162a7a1e84b06
nikmihalevich <nikandr33@inbox.ru>
2023-09-03 01:11:54 +0300

    redesign article details, fixes

---
+++ src/entities/Article/ui/ArticleDetails/renderArticleBlock.tsx
@@ -0,0 +1,38 @@
+import cls from './ArticleDetails.module.scss';
+import { ArticleBlockType } from '../../model/consts/articleConsts';
+import { ArticleBlock } from '../../model/types/article';
+import { ArticleCodeBlockComponent } from '../ArticleCodeBlockComponent/ArticleCodeBlockComponent';
+import { ArticleImageBlockComponent } from '../ArticleImageBlockComponent/ArticleImageBlockComponent';
+import { ArticleTextBlockComponent } from '../ArticleTextBlockComponent/ArticleTextBlockComponent';
+
+export const renderArticleBlock = (block: ArticleBlock) => {
+	switch (block.type) {
+		case ArticleBlockType.TEXT:
+			return (
+				<ArticleTextBlockComponent
+					key={block.id}
+					block={block}
+					className={cls.block}
+				/>
+			);
+		case ArticleBlockType.IMAGE:
+			return (
+				<ArticleImageBlockComponent
+					key={block.id}
+					block={block}
+					className={cls.block}
+				/>
+			);
+		case ArticleBlockType.CODE:
+			return (
+				<ArticleCodeBlockComponent
+					key={block.id}
+					block={block}
+					className={cls.block}
+				/>
+			);
+
+		default:
+			return null;
+	}
+};
